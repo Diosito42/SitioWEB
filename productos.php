cb2c0a8f8f639195e2ad647197816e67dbab723f
Papita <you@example.com>
2022-02-16 01:54:00 -0600

    creacion del proyecto sitio web

---
+++ productos.php
@@ -0,0 +1,52 @@
+<?php include("template/cabecera.php"); ?>
+
+<div class="col-md-3">
+	<div class="card">
+		<img class="card-img-top" src="https://www.gratistodo.com/wp-content/uploads/2021/12/Libros-Wallpapers-1-390x220.jpg" alt="">
+
+		<div class="card-body">
+			<h4 class="card-title">Libro</h4>
+			<a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas
+			</a>
+		</div>
+	</div> 
+</div>
+
+<div class="col-md-3">
+	<div class="card">
+	<img class="card-img-top" src="https://www.gratistodo.com/wp-content/uploads/2021/12/Libros-Wallpapers-1-390x220.jpg" alt="">
+
+		<div class="card-body">
+			<h4 class="card-title">Libro</h4>
+			<a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas
+			</a>
+		</div>
+	</div>
+</div>
+
+<div class="col-md-3">
+	<div class="card">
+	<img class="card-img-top" src="https://www.gratistodo.com/wp-content/uploads/2021/12/Libros-Wallpapers-1-390x220.jpg" alt="">
+
+		<div class="card-body">
+			<h4 class="card-title">Libro</h4>
+			<a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas
+			</a>
+		</div>
+	</div>
+</div>
+
+<div class="col-md-3">
+	<div class="card">
+	<img class="card-img-top" src="https://www.gratistodo.com/wp-content/uploads/2021/12/Libros-Wallpapers-1-390x220.jpg" alt="">
+
+		<div class="card-body">
+			<h4 class="card-title">Libro</h4>
+			<a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas
+			</a>
+		</div>
+	</div>
+</div>
+
+
+<?php include("template/pie.php"); ?>
